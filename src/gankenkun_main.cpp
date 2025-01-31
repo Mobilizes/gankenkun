1b3b6622a98a20f586232507de4082e031798e8a
FaaizHaikal <faaizhaikal@gmail.com>
2025-01-31 14:05:47 +0700

    feat: main program

---
+++ src/gankenkun_main.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2025 ICHIRO ITS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+
+#include "gankenkun/gankenkun.hpp"
+
+int main(int argc, char * argv[])
+{
+  auto args = rclcpp::init_and_remove_ros_arguments(argc, argv);
+
+  const std::string & path = args[1];
+
+  auto node = std::make_shared<rclcpp::Node>("gankenkun");
+  auto gankenkun_node = std::make_shared<gankenkun::GankenkunNode>(node);
+
+  auto walking_manager = std::make_shared<gankenkun::WalkingManager>(path);
+  walking_manager->load_config(path);
+
+  gankenkun_node->set_walking_manager(walking_manager);
+  gankenkun_node->run_config_service(path);
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+  return 0;
+}
