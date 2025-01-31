458a0ec7a81eb1d7be2a61c3c5b95f948b5909a2
FaaizHaikal <faaizhaikal@gmail.com>
2025-01-31 13:25:21 +0700

    feat: walking node

---
+++ include/gankenkun/walking/node/walking_node.hpp
@@ -0,0 +1,64 @@
+// Copyright (c) 2025 Ichiro ITS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef GANKENKUN__WALKING__NODE__WALKING_NODE_HPP_
+#define GANKENKUN__WALKING__NODE__WALKING_NODE_HPP_
+
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+
+#include "gankenkun/walking/node/walking_manager.hpp"
+
+namespace gankenkun
+{
+
+class WalkingNode
+{
+public:
+  using SetJoints = tachimawari_interfaces::msg::SetJoints;
+  using WalkingStatus = gankenkun_interfaces::msg::WalkingStatus;
+  using Point2 = gankenkun_interfaces::msg::Point2;
+  using KanseiStatus = kansei_interfaces::msg::Status;
+  using SetWalking = gankenkun_interfaces::msg::SetWalking;
+
+  WalkingNode(
+    const rclcpp::Node::SharedPtr & node, const std::shared_ptr<WalkingManager> & walking_manager);
+
+  void update();
+
+private:
+  void publish_joints();
+  void publish_status();
+
+  std::shared_ptr<WalkingManager> walking_manager;
+
+  rclcpp::Node::SharedPtr node;
+
+  rclcpp::Subscription<Point2>::SharedPtr set_walking_subscriber;
+  rclcpp::Subscription<Point2>::SharedPtr set_odometry_subscriber;
+
+  rclcpp::Publisher<SetJoints>::SharedPtr set_joints_publisher;
+  rclcpp::Publisher<WalkingStatus>::SharedPtr status_publisher;
+};
+
+}  // namespace gankenkun
+
+#endif  // GANKENKUN__WALKING__NODE__WALKING_NODE_HPP_
---
+++ src/gankenkun/walking/node/walking_node.cpp
@@ -0,0 +1,90 @@
+// Copyright (c) 2025 Ichiro ITS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "gankenkun/walking/node/walking_node.hpp"
+
+namespace gankenkun
+{
+
+WalkingNode::WalkingNode(
+  const rclcpp::Node::SharedPtr & node, const std::shared_ptr<WalkingManager> & walking_manager)
+: node(node), walking_manager(walking_manager)
+{
+  set_walking_subscriber = node->create_subscription<SetWalking>(
+    "walking/set_walking", 10, [this](const SetWalking::SharedPtr message) {
+      if (message->run) {
+        this->walking_manager->set_goal(
+          keisan::Point2(message->x, message->y), keisan::Angle<double>(message->a));
+      } else {
+        this->walking_manager->stop();
+      }
+    });
+
+  set_odometry_subscriber = node->create_subscription<Point2>(
+    "walking/set_odometry", 10, [this](const Point2::SharedPtr message) {
+      this->walking_manager->set_position(keisan::Point2(message->x, message->y));
+    });
+
+  orientation_subscriber = node->create_subscription<KanseiStatus>(
+    "measurement/status", 10, [this](const KanseiStatus::SharedPtr message) {
+      this->walking_manager->update_orientation(keisan::make_degree(message->orientation.yaw));
+    });
+
+  status_publisher = node->create_publisher<WalkingStatus>("walking/status", 10);
+
+  set_joints_publisher = node->create_publisher<SetJoints>("joint/set_joints", 10);
+}
+
+void WalkingNode::update()
+{
+  publish_joints();
+  publish_status();
+}
+
+void WalkingNode::publish_joints()
+{
+  auto joints_msg = SetJoints();
+
+  const auto & joints = walking_manager->get_angles();
+  auto & joint_msgs = joints_msg.joints;
+
+  joint_msgs.resize(joints.size());
+  for (size_t i = 0; i < joints.size() && i < joint_msgs.size(); ++i) {
+    joint_msgs[i].id = joints[i].get_id();
+    joint_msgs[i].position = joints[i].get_position();
+  }
+
+  joints_msg.control_type = tachimawari::joint::Middleware::FOR_WALKING;
+
+  set_joints_publisher->publish(joints_msg);
+}
+
+void WalkingNode::publish_status()
+{
+  auto status_msg = WalkingStatus();
+
+  status_msg.status = walking_manager->get_status();
+  status_msg.odometry.x = walking_manager->get_position().x;
+  status_msg.odometry.y = walking_manager->get_position().y;
+
+  status_publisher->publish(status_msg);
+}
+
+}  // namespace gankenkun
